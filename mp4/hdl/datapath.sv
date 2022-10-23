6065c16133a1d31dec23c9b8f896fac9e582a172
Jellyyz <gallyhuang@gmail.com>
2022-10-22 19:07:42 -0500

    added some aluop

--- mp4/hdl/datapath.sv
+++ mp4/hdl/datapath.sv
@@ -11,19 +11,12 @@ module datapath(
 logic [6:0] opcode; 
 logic [2:0] funct3; 
 logic [6:0] funct7; 
-
-// opcode of any instruction 
-assign opcode = instr_mem_rdata[6:0]; 
-
-// funct3 of any instruction 
-assign funct3 = instr_mem_rdata[2:0]; 
-
-// funct7 of any instruction 
-assign funct7 = instr_mem_rdata[6:0]; 
+alu_ops aluop;
 
 // control signals from the control block ?
 
 logic load_pc, pcmux_out, pc_out, pcmux_sel; 
+logic alu_op; 
 
 pc_register pc_register(
     // inputs 
@@ -45,6 +38,63 @@ register MDR(
 
 ); 
 
+alu alu (
+    // inputs 
+    .aluop(aluop),
+    .a(alumux1_out), .b(alumux2_out), 
+    // outputs 
+    .f(alu_out)
+    
+);
+
+
+always_comb begin : CONTROL_WORD
+
+
+    // opcode of any instruction 
+    opcode = instr_mem_rdata[6:0]; 
+
+    // funct3 of any instruction 
+    funct3 = instr_mem_rdata[2:0]; 
+
+    // funct7 of any instruction 
+    funct7 = instr_mem_rdata[6:0]; 
+
+    unique case(funct3)
+        sll, axor, aor, aand:begin
+            alu_op = funct3; 
+        end 
+
+        slt:begin 
+            alu_op = blt;
+        end 
+
+        sltu:begin 
+            alu_op = bltu; 
+        end 
+
+        sr:begin 
+            if(funct7 == 7'h0)begin 
+                alu_op = alu_srl;
+            end 
+            else begin 
+                alu_op = alu_sra; 
+            end 
+        end 
+        
+        add:begin 
+            if(funct7 == 7'h0)begin 
+                alu_op = alu_add; 
+            end 
+            else begin 
+                alu_op = alu_sub; 
+            end 
+        end     
+
+    endcase 
+
+
+end 
 always_comb begin : MUXES
 
 unique case(pcmux_sel) 
