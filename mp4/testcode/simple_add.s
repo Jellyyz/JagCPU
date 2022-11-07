ad93efd6464b7a4eddc19b8eb6c6faf113c8ffbb
Arjun Ray <arjunr4@illinois.edu>
2022-11-06 21:08:49 -0600

    adding simple test

---
+++ mp4/testcode/simple_add.s
@@ -0,0 +1,43 @@
+
+.align 4
+.section .text
+.globl _start
+_start:
+pcrel_NEGTWO: auipc x10, %pcrel_hi(NEGTWO)
+pcrel_TWO: auipc x11, %pcrel_hi(TWO)
+pcrel_ONE: auipc x12, %pcrel_hi(ONE)
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    lw x2, %pcrel_lo(pcrel_TWO)(x11)
+    lw x1, %pcrel_lo(pcrel_ONE)(x12)
+    nop
+    nop
+    nop
+    add x2, x2, x2   
+    add x1, x1, x1
+
+.section .rodata
+.balign 256
+ONE:    .word 0x00000001
+TWO:    .word 0x00000002
+NEGTWO: .word 0xFFFFFFFE
+TEMP1:  .word 0x00000001
+GOOD:   .word 0x600D600D
+BADD:   .word 0xBADDBADD
+BYTES:  .word 0x04030201
+HALF:   .word 0x0020FFFF
+
+HALT:
+    beq x0, x0, HALT
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
